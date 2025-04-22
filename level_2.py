e2bff85f0820e80d4754d4c262e9326dcf79fea3
ying4real <jchang12@berklee.edu>
2025-04-22 18:54:08 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,4 @@
+playlist_for_coding = ["Heartbeat by Don Johnson", "Promises, Promises by Naked Eyes",
+                       "Only A Fool Would Say That by Steely Dan", "Dreams by Fleetwood Mac",
+                       "Kokomo by The Beach Boys"]
+playlist_for_coding.append ("Never Gonna Give You Up by Rick Ashley")
